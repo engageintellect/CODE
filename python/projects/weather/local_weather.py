7f4e87632f81db079faf5a6eb3f5ea1d58bd40d0
jc9361 <jc9361@gmail.com>
2020-10-19 03:29:50 -0700

    cleaning up

--- python/projects/weather/local_weather.py
+++ python/projects/weather/local_weather.py
@@ -11,7 +11,7 @@ owm = pyowm.OWM('e976803d50a2aeec31e749c537477b34')
 mgr = owm.weather_manager()
 
 
-location = input('Please enter a location: ')
+location = 'Irvine, US'
 subprocess.call('clear', shell=True)
 
 

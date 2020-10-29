4bb663a148768ecd98b91a9c7cb6edc3ed8c6ce0
jc9361 <jc9361@gmail.com>
2020-10-28 23:33:50 -0700

    updating todolist

---
+++ web/todolist/server.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./static/"))) //replace "satic" with dir name
+	http.ListenAndServe(":3000", nil)                        //3000 can be replaced with any other avaiable port number
+
+}

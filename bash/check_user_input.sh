1e522ff3ff5c0e5e108d9cb8017950f8b54cf7b1
jc9361 <jc9361@gmail.com>
2020-11-21 23:28:56 -0800

    updating repo

---
+++ bash/check_user_input.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo -n "enter a number: "
+read num1
+
+if [ $num1 == 10 ]; then echo its ten; else echo its not ten; fi
+
+
+
+
+
---
+++ bash/user_input.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# taking user input using "echo -n" and read functions.
+echo -n "enter name: "
+read name
+echo your name is $name
+
+# taking user input using "read -p" functions
+read -p "enter your age: " age
+echo you are $age
+
+

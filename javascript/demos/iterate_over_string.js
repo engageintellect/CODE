0e473b371362cde632af41e564132230070639c9
jc9361 <jc9361@gmail.com>
2021-01-10 23:03:04 -0800

    updating

--- javascript/demos/iterate_over_string.js
+++ javascript/demos/iterate_over_string.js
@@ -1,5 +1,15 @@
 str = 'hello world'
 
 for(let x=0; x<str.length; x++){
-    console.log(str[x]);
+    console.log(str[x])
+}
+
+
+
+
+
+list = ['hello', 'bye', 'later']
+
+for(let x=0; x<list.length; x++){
+    console.log(list[x])
 }

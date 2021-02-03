0e03df54d847b32b024a1bd5e955a6fd4fe88835
jc9361 <jc9361@gmail.com>
2021-02-02 16:21:33 -0800

    initial commit

--- python/projects/stock_tools/check_price/main.py
+++ python/projects/stock_tools/check_price/main.py
@@ -3,16 +3,19 @@ import yfinance as yf
 import datetime
 from time import sleep
 
+
 os.chdir('/home/r3dux/code/python/projects/stock_tools/check_price')
 def display():
     os.system('clear')
     os.system('figlet -f mini "S T O C K   P R I C E S" | boxes -d simple')
     print("\n")
 
+
 def get_current_price(symbol):
     ticker = yf.Ticker(symbol)
     todays_data = ticker.history(period='1d')
     return todays_data['Close'][0]
+
 
 def get_watchlist():
     watchlist = {}
@@ -31,7 +34,6 @@ def get_watchlist():
     for ticker, price in watchlist.items():
         print(f'{"    "}{ticker:<8} ${price}')
     
-
 
     print('\n')
 
---
+++ scratch/test.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+
+
+def display():
+    os.system('figlet OPTIONS')
+
+
+
+def figger(word):
+    os.system(f'figlet {word}')
+
+
+
+
+
+
+def main():
+    display()
+
+
+main()

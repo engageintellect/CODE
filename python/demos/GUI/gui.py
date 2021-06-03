670d80a16c920ba5d0ff498304729dac2c1b555a
jc9361 <jc9361i@gmail.com>
2021-06-02 19:05:40 -0700

    updating code folder

---
+++ python/demos/GUI/gui.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import PySimpleGUI as sg
+import requests
+
+
+layout = [[sg.Text("Hello World")], [sg.Button("OK")]]
+
+# Create the window
+window = sg.Window("Demo", layout)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()

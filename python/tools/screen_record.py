c056552844fe04a0c0e3a15f63b0e10d646f463e
jc9361 <jc9361i@gmail.com>
2021-08-24 19:47:22 -0700

    adding screen_record.py

---
+++ python/tools/screen_record.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+import os
+import subprocess
+
+screen_res = 0
+
+
+def clear_screen():
+    os.system('clear && figlet "screen_record"')
+    print('\n')
+
+
+def get_resolution():
+    res = subprocess.getoutput("xrandr | grep connected | grep primary | awk '{print $4}'")
+    res = res.replace('+0+0', '')
+    return res
+
+
+def record_screen():
+    file_out = input("NAME VIDEO OUTPUT: ")
+    res = get_resolution()
+    cmd = f'ffmpeg -f x11grab -s {res} -r 25 -i :0.0 {file_out}.mp4'
+    os.system(cmd)
+
+
+def main():
+    clear_screen()
+    record_screen()
+main()

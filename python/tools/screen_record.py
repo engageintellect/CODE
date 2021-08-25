96f6209e782955524105927d5c55df19173049a4
jc9361 <jc9361i@gmail.com>
2021-08-25 00:41:01 -0700

    cleaning up

--- python/tools/screen_record.py
+++ python/tools/screen_record.py
@@ -7,17 +7,17 @@ import subprocess
 screen_res = 0
 
 
+# CLEAR THE SCREEN
 def clear_screen():
     os.system('clear && figlet "SCREEN RECORD"')
     print('\n')
 
-
+# GET DISPLAY RESOLUTION
 def get_resolution():
-    res = subprocess.getoutput("xrandr | grep connected | grep primary | awk '{print $4}'")
-    res = res.replace('+0+0', '')
+    res = subprocess.getoutput("xrandr | grep connected | grep primary | awk '{print $4}' | sed 's/+0+0//g'")
     return res
 
-
+# RECORD THE SCREEN
 def record_screen():
     file_out = input("NAME VIDEO OUTPUT: ")
     if file_out != 'q':
@@ -27,11 +27,15 @@ def record_screen():
         quit()
 
     res = get_resolution()
+    path = subprocess.getoutput('echo $HOME')
+    os.chdir(path)
     cmd = f'ffmpeg -f x11grab -s {res} -r 25 -i :0.0 {file_out}.mp4'
     os.system(cmd)
 
 
-def main():
+# MAIN RUN
+if __name__ == "__main__":
     clear_screen()
     record_screen()
-main()
+
+

a4b43abe474039a2adc29fe3cd4a0105757f303f
jc9361 <jc9361@gmail.com>
2022-01-31 03:03:03 -0800

    adding class method chaining demo

---
+++ javascript/demos/class_method_chaining.js
@@ -0,0 +1,29 @@
+class User{
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+        this.score = 0;
+    }
+
+    login(){
+        console.log(this.email, 'just logged in');
+        return this;
+    }
+
+    logout(){
+        console.log(this.email, 'just logged out');
+        return this;
+    }
+    
+    update_score(){
+        this.score++;
+        console.log(this.email, `score is now ${this.score}`);
+        return this;
+
+    }   
+}
+
+var userOne = new User("josh", "jc9361@gmail.com")
+var userTwo = new User("jim", "jim@gmail.com")
+
+userOne.login().update_score().update_score().logout();
